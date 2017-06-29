dc5dd485ae307774619a5b798c4af5fe09c2f3ff
FeifanBoy <961010504@qq.com>
2017-06-29 15:26:25 +0800

    登录功能部分界面及基本交互

--- app/src/main/java/com/healthyfish/healthyfish/ui/presenter/login/LoginPresenter.java
+++ app/src/main/java/com/healthyfish/healthyfish/ui/presenter/login/LoginPresenter.java
@@ -1,5 +1,12 @@
 package com.healthyfish.healthyfish.ui.presenter.login;
 
+import android.os.Handler;
+
+import com.healthyfish.healthyfish.model.login.ILoginModel;
+import com.healthyfish.healthyfish.model.login.LoginModel;
+import com.healthyfish.healthyfish.model.login.OnLoginListener;
+import com.healthyfish.healthyfish.ui.view.login.ILoginView;
+
 /**
  * 描述：登录Presenter
  * 作者：Wayne on 2017/6/28 11:16
@@ -10,4 +17,44 @@ package com.healthyfish.healthyfish.ui.presenter.login;
 
 // TODO: 2017/6/28 实现登录Presenter
 public class LoginPresenter {
+
+    private ILoginView loginView;
+    private ILoginModel loginModel;
+    private Handler mHandler = new Handler();
+
+    public LoginPresenter(ILoginView loginView){
+        this.loginView = loginView;
+        this.loginModel = new LoginModel();
+    }
+
+    public void login(){
+        loginView.showProgressBar();
+        loginModel.Login(loginView.getUserName(), loginView.getPassword(), new OnLoginListener() {
+            @Override
+            public void LoginSucess() {
+                mHandler.post(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        loginView.hideProgressBar();
+                        loginView.toActivity();
+                    }
+                });
+            }
+
+            @Override
+            public void LoginFailed() {
+                mHandler.post(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        loginView.hideProgressBar();
+                        loginView.showFailedError();
+                    }
+                });
+            }
+        });
+    }
 }
