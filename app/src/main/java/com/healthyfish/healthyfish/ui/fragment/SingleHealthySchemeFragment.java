d0ac3b204a551b98c6a57fa231100e0a8da6808d
FeifanBoy <961010504@qq.com>
2017-07-16 21:22:21 +0800

    7.16 (#9)
    * Merge pull request #16 from TMXKT/master (#10)
    
    * 挂号模块添加了选择预约时间界面
    
    * Merge pull request #18 from TMXKT/master (#11)
    
    * 完善了挂号模块选择预约时间功能，添加了安卓版本6.0以上的获取权限方法
    
    * 7.13 (#12)
    
    * 完成了体质测试模块页面基本逻辑，目前红外皮温选择体质部分仍有bug
    
    * 完成了体质测试模块页面基本逻辑，修复红外皮温选择体质部分bug，添加挂号模块选择科室和科室医生页面到入口之下
    
    * 完成了体质测试模块页面基本逻辑，修复红外皮温选择体质部分bug，添加挂号模块选择科室和科室医生页面到入口之下，在activity包下添加了几个包将activity分类
    
    * 完成了体质测试模块页面基本逻辑，修复红外皮温选择体质部分bug，添加挂号模块选择科室和科室医生页面到入口之下，在activity包下添加了几个包将activity分类
    
    * 完成了体质测试模块页面基本逻辑，修复红外皮温选择体质部分bug，添加挂号模块选择科室和科室医生页面到入口之下，在activity包下添加了几个包将activity分类
    
    * 完成了体质测试模块页面基本逻辑，修复红外皮温选择体质部分bug，添加挂号模块选择科室和科室医生页面到入口之下，在activity包下添加了几个包将activity分类
    
    * 解决健康圈首页数据加载问题
    
    * 修改了登录注册界面的输入框输入属性，问诊首页的布局，送心意页面弹出输入框后顶部toolbar被顶上去的现象
    
    * 合并 (#1)
    
    * Merge pull request #16 from TMXKT/master (#10)
    
    * 挂号模块添加了选择预约时间界面
    
    * Merge pull request #18 from TMXKT/master (#11)
    
    * 完善了挂号模块选择预约时间功能，添加了安卓版本6.0以上的获取权限方法
    
    * 修改了登录注册界面的输入框输入属性，问诊首页的布局，送心意页面弹出输入框后顶部toolbar被顶上去的现象
    
    * 版本1.2.15：
    2017.07.14
    1. 添加健康管理ViewPage
    
    * 测试了原生视频播放器
    
    * 合并
    
    * 版本1.2.15：
    2017.07.14.2
    1. 添加健康管理ViewPage

--- app/src/main/java/com/healthyfish/healthyfish/ui/fragment/SingleHealthySchemeFragment.java
+++ app/src/main/java/com/healthyfish/healthyfish/ui/fragment/SingleHealthySchemeFragment.java
@@ -1,4 +1,4 @@
-package com.healthyfish.healthyfish.ui.activity.healthy_management;
+package com.healthyfish.healthyfish.ui.fragment;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -49,6 +49,6 @@ public class SingleHealthySchemeFragment extends Fragment {
         HealthySchemeAdapter adapter = new HealthySchemeAdapter(mContext, listHealthyScheme);
         healthySchemeRecyclerview.setAdapter(adapter);
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return v;
     }
 }
